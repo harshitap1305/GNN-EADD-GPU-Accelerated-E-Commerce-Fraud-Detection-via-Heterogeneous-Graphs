0e7a07b199db9fbda2468c601b6d16741b205c9c
snehalsuhane <142654825+snehalsuhane@users.noreply.github.com>
2026-04-14 16:47:05 +0530

    Rename package from 'custom_spmm' to 'gnn_eadd_extensions'

--- cuda_spmm/setup.py
+++ cuda_spmm/setup.py
@@ -2,10 +2,13 @@ from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 setup(
-    name='custom_spmm',
+    name='gnn_eadd_extensions',
     ext_modules=[
         CUDAExtension('custom_spmm', [
             'spmm_kernel.cu',
+        ]),
+        CUDAExtension('warp_gat', [
+            'warp_gat_kernel.cu',
         ])
     ],
     cmdclass={
